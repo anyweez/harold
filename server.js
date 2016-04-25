7aec7687ae581e9b6d3ba9aa88b6199e282a3c29
Luke Segars <luke@theironyard.com>
2016-04-25 12:24:42 -0400

    Adding news providers.

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 const path = require('path');
 const hapi = require('hapi');
 const inert = require('inert');
+// const ws = require('ws');
+const websocket = require('hapi-plugin-websocket');
 const messagelog = require('./messagelog');
+const news = require('./news');
 
 const host = '0.0.0.0';
 const port = process.env.PORT || 7000;
@@ -23,18 +26,30 @@ server.connection({
     port: port,
 });
 
-server.register(inert, function () {});
+server.register(inert, function () { });
 
-// 
+// const socketServer = new ws.Server({
+//     server: server,
+// })
+
+// socketServer.broadcast = function (data) {
+//     socketServer.clients.forEach(function (client) {
+//         client.send(JSON.stringify(data));
+//     });
+// };
+
 var routes = [
     require('./routes/get-messages'),
     require('./routes/post-messages'),
-    require('./routes/get-stocks'),
     require('./routes/get-trinkets'),
+    require('./routes/get-exchange-rates'),
+    require('./routes/get-news-providers'),
+    require('./routes/get-news-stories'),
 ];
 
 var shared_state = {
     messages: messagelog.new(1000),
+    news: news.new(),
 };
 // Put an initial message in there so the log is never empty.
 shared_state.messages.add('luke', 'guess whoooooo');
@@ -44,6 +59,27 @@ shared_state.messages.add('luke', 'guess whoooooo');
  */
 routes.forEach(function (route) {
     server.route(route(shared_state));
+});
+
+server.register(websocket, function () {
+    server.route({
+        method: 'POST',
+        path: '/api/feed',
+        config: {
+            plugins: {
+                websocket: {
+                    only: true,
+                    connect: function (wss, ws) {
+                        shared_state.news.notify = wss.clients;
+                        ws.send(JSON.stringify({ msg: 'youre here' }));
+                    },
+                }
+            },
+        },
+        handler: function (request, reply) {
+            
+        },
+    })
 });
 
 // Fallback route in case none of the other routes match.
@@ -58,7 +94,6 @@ server.route({
         }
     },
 });
-
 
 server.start(function (error) {
     if (error) {
