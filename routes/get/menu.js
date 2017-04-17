8dcd658ba1decb2c5127c34d90dec4814cf828fb
Luke Segars <luke@lukesegars.com>
2017-04-17 15:08:04 -0400

    Adding restaurant menu.

---
+++ routes/get/menu.js
@@ -0,0 +1,12 @@
+/**
+ * @api {get} /chats Get list of all available chat messages
+ * @apiName GetChats
+ * @apiGroup Chats
+ * 
+ * @apiSuccess {Array} chats    All available chat messages
+ */
+module.exports = {
+    handler(request, reply, state) {
+        reply(state.restaurant.items);
+    },
+};
---
+++ state/restaurant.js
@@ -0,0 +1,45 @@
+let next_food_id = 0;
+
+function Food(name, description, price) {
+    this.id = next_food_id++;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.available = true;
+
+    return this;
+}
+
+function Restaurant() {
+    this.orders = {};
+    this.items = [
+        new Food('Caesar Salad', 'An extraordinary mix of lettuce and croutons.', 8.99),
+        new Food('Pickled Asparagus', 'A local favorite; just what it sounds like.', 11.39),
+        new Food('Carrot Smoothie', 'Fresh from the garden.', 4.19),
+    ];
+
+    this.items[2].available = false;
+
+    return this;
+}
+
+/* Add an order  */
+Restaurant.prototype.add_to = function (table_id, item) {
+    if (!this.exists(table_id)) {
+        this.orders[table_id] = [];
+    }
+
+    this.orders[table_id].push(this.get_item(item));
+};
+
+/* Does order already exist? */
+Restaurant.prototype.exists = function (table_id) {
+    return this.orders.hasOwnProperty(table_id);
+};
+
+/* Check to see if the specified item exists; return if so */
+Restaurant.prototype.get_item = function (id) {
+    return this.items.find(i => i.id === id);
+};
+
+module.exports = Restaurant;
